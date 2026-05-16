737fa944ffb5528614f66440195ccba2d0bbcd26
ryanlui1222 <64682301+ryanlui1222@users.noreply.github.com>
2026-05-16 20:32:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def hanase_ai_process(word):
     
     try:
         response = client.models.generate_content(
-            model='gemini-2.5-flash',
+            model='gemini-3.1-flash-lite',
             contents=prompt,
             config=types.GenerateContentConfig(
                 response_mime_type="application/json"
